565cb1fe17470f32fb83deaf3987464969cc579e
hisho <hisho.web@gmail.com>
2023-06-16 22:58:16 +0900

    ✅ test

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,6 +1,6 @@
 import { NEXT_PUBLIC_FRONTEND_URL } from '@/constant/constant'
 
-export const fetchArticles = async (): Promise<{ message: string }> =>
+const fetchArticles = async (): Promise<{ message: string }> =>
   fetch(`${NEXT_PUBLIC_FRONTEND_URL}/articles/api`).then((res) => res.json())
 
 const Page = async () => {
