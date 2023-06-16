d5796589455ab8bd9f63a8cc20bf75f09b2548bd
hisho <hisho.web@gmail.com>
2023-06-16 23:27:54 +0900

    ✅ test

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,7 +1,8 @@
 import { FRONTEND_URL } from '@/constant/constant'
+import { env } from '@/constant/env'
 
 const fetchArticles = async (): Promise<{ message: string }> =>
-  fetch(`${FRONTEND_URL}/articles/api`).then((res) => res.json())
+  fetch(`https://${env.VERCEL_URL}/articles/api`).then((res) => res.json())
 
 const Page = async () => {
   const { message } = await fetchArticles()
